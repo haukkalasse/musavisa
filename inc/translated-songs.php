4803edbd32d3f135c7e42de32dd079e871eaa6e0
haukkalasse <haukkalasse@gmail.com>
2024-07-25 22:58:48 +0300

    Translated songs array

---
+++ inc/translated-songs.php
@@ -0,0 +1,134 @@
+<?php
+//Translated songs in array
+$translated_songs = array(
+	array(
+		'name'              => 'Smash Mouth - All Star',
+		'file'              => 'All Star.mp3',
+		'tts'               => 'All Star.mp3',
+		'lyrics_original'   => 'Somebody once told me the world is gonna roll me<br>I ain\'t the sharpest tool in the shed<br>She was looking kind of dumb with her finger and her thumb<br>In the shape of an "L" on her forehead<br><br>Well, the years start comin\' and they don\'t stop comin\'<br>Fed to the rules and I hit the ground runnin\'<br>Didn\'t make sense not to live for fun<br>Your brain gets smart but your head gets dumb<br>So much to do, so much to see<br>So what\'s wrong with taking the backstreets?<br>You\'ll never know if you don\'t go (W-w-wacko)<br>You\'ll never shine if you don\'t glow<br><br>Hey now, you\'re an all star<br>Get your game on, go play<br>Hey now, you\'re a rock star<br>Get the show on, get paid<br>(And all that glitters is gold)<br>Only shootin\' stars break the mold',
+		'lyrics_translated' => 'Joku kerran sanoi minulle, että maailma tulee rullaamaan minua<br>En ole terävin työkalu vajassa<br>Hän näytti aika tyhmältä sormineen ja peukaloineen<br>L-kirjaimen muotoisena otsassa<br><br>No, vuodet alkavat tulla, eivätkä ne lopu tulemasta<br>Kyllästyneenä sääntöihin ja aloin juosta<br>Ei ollut järkeä olla elämättä hauskanpidosta<br>Aivot viisastuu, mutta pää tyhmyy<br>Niin paljon tehtävää, niin paljon nähtävää<br>Joten mitä vikaa siinä on, että kulkee sivuteitä?<br>Et ikinä tiedä, jos et mene (W-w-wacko)<br>Et koskaan loista, jos et hehkuta<br><br>Hei nyt, olet kaikkitähti<br>Mene leikkimään, mene leikkimään<br>Hei nyt, olet rocktähti<br>Mene keikalle, saat palkkaa<br>Ja kaikki kimaltava on kultaa<br>Vain ampuvat tähdet rikkovat muotin',
+	),
+	array(
+		'name'              => 'Eminem - Lose yourself',
+		'file'              => 'Lose Yourself.mp3',
+		'tts'               => 'Lose Yourself.mp3',
+		'lyrics_original'   => 'Look, if you had one shot or one opportunity<br>To seize everything you ever wanted in one moment<br>Would you capture it or just let it slip?<br>Yo<br><br>His palms are sweaty, knees weak, arms are heavy<br>There\'s vomit on his sweater already, mom\'s spaghetti<br>He\'s nervous, but on the surface, he looks calm and ready<br>To drop bombs, but he keeps on forgetting<br>What he wrote down, the whole crowd goes so loud<br>He opens his mouth, but the words won\'t come out',
+		'lyrics_translated' => 'Jos sinulla olisi yksi tilaisuus tai mahdollisuus<br>Saada kaikki mitä ikinä halusit yhdessä hetkessä<br>Ottaisitko sen kiinni vai antaisitko sen vain lipsahtaa?<br>Jou<br><br>Hänen kämmenensä ovat hikiset, polvet heikot, kädet raskaat<br>Puserossa on jo oksennusta, äidin spagettia<br>Hän on hermostunut, mutta päällisin puolin hän näyttää rauhalliselta ja valmiilta.<br>Pommien pudottamiseen, mutta hän unohtaa sen.<br>Mitä hän kirjoitti muistiin, koko yleisö on niin äänekäs.<br>Hän avaa suunsa, mutta sanat eivät tule ulos',
+	),
+	array(
+		'name'              => 'Kiss - Love Gun',
+		'file'              => 'Love Gun.mp3',
+		'tts'               => 'Love Gun.mp3',
+		'lyrics_original'   => 'I really love you baby, I love what you\'ve got<br>Let\'s get together, we can get hot<br>No more tomorrow, baby time is today<br>Girl, I can make you feel okay<br><br>No place for hidin\' baby, no place to run<br>You pull the trigger of my<br>Love gun, (love gun), love gun<br>Love gun, (love gun), love gun<br><br>You can\'t forget me baby, don\'t try to lie<br>You\'ll never leave me, mama, so don\'t try<br>I\'ll be a gambler, baby lay down the bet<br>We get together, mama you\'ll sweat',
+		'lyrics_translated' => 'Rakastan sinua todella, kulta, rakastan sitä mitä sinulla on<br>Mennään yhteen, voimme tulla kuumiksi<br>Ei enää huomista, beibi aika on tänään<br>Tyttö minä voin saada sinut tuntemaan hyväksi<br><br>Ei ole paikkaa, minne piiloutua, ei paikkaa, minne paeta<br>Sinä painat liipaisimestani<br>Rakkauspyssy, (rakkauspyssy), rakkauspyssy<br>Rakkauspyssy, (rakkauspyssy), rakkauspyssy<br><br>Et voi unohtaa minua beibi, älä yritä valehdella<br>Et koskaan jätä minua, äiti, joten älä yritä<br>Olen uhkapeluri, kulta, laita veto pois<br>Me menemme yhteen, äiti sinä hikoilet',
+	),
+	array(
+		'name'              => 'Rammstein - Du Hast',
+		'file'              => 'Du Hast.mp3',
+		'tts'               => 'Du Hast.mp3',
+		'lyrics_original'   => 'Du, du hast, du hast mich<br>Du, du hast, du hast mich<br>Du, du hast, du hast mich<br>Du, du hast, du hast mich<br><br>Du, du hast, du hast mich, du hast mich<br>Du hast mich gefragt, du hast mich gefragt<br>Du hast mich gefragt und ich hab nichts gesagt<br><br>Willst du bis der Tod euch scheidet<br>Treu ihr sein für alle Tage?<br>Nein!<br>Nein!<br><br>Willst du bis der Tod euch scheidet<br>Treu ihr sein für alle Tage?<br>Nein!<br>Nein!',
+		'lyrics_translated' => 'Sinä, sinulla on, sinulla on minut<br>Sinä, sinulla on, sinulla on minut<br>Sinä, sinulla on, sinulla on minut<br>Sinä, sinulla on, sinulla on minut<br><br>Sinulla on minut, sinulla on minut, sinulla on minut<br>Kysyit minulta, kysyit minulta<br>Kysyit minulta, enkä sanonut mitään<br><br>Oletko uskollinen, kunnes kuolema teidät erottaa?<br>Oletko uskollinen hänelle ikuisesti?<br>Ei!<br>Ei!<br><br>Oletko uskollinen, kunnes kuolema teidät erottaa?<br>Oletko uskollinen hänelle ikuisesti?<br>Ei!<br>Ei!',
+	),
+	array(
+		'name'              => 'The Runaways - Cherry Bomb',
+		'file'              => 'Cherry Bomb.mp3',
+		'tts'               => 'Cherry Bomb.mp3',
+		'lyrics_original'   => 'Can\'t stay at home, can\'t stay in school<br>Old folks say, You poor little fool<br>Down the streets I\'m the girl next door<br>I\'m the fox you\'ve been waiting for<br><br>Hello, daddy, hello, mom<br>I\'m your ch-ch-ch-cherry bomb<br>Hello world I\'m your wild girl<br>I\'m your ch-ch-ch-cherry bomb<br><br>Stone Age love and strange sounds too<br>Come on, baby, let me get to you<br>Bad nights causing teenage blues<br>Get down ladies, you\'ve got nothin\' to lose',
+		'lyrics_translated' => 'Ei voi jäädä kotiin, ei voi jäädä kouluun<br>Vanhukset sanovat: Sinä pikku hölmöparka<br>Kaduilla olen naapurin tyttö<br>Olen kettu, jota olet odottanut<br><br>Hei, isä, hei, äiti<br>Mä oon sun kirsikkapommi<br>Hei maailma, olen villi tyttösi<br>Olen sinun k-k-k-kirsikkapommisi<br><br>Kivikauden rakkautta ja outoja ääniä<br>Anna tulla, kulta, anna minun tulla luoksesi<br>Huonot yöt aiheuttavat teinibluesin<br>Menkää alas naiset, teillä ei ole menetettävää',
+	),
+	array(
+		'name'              => 'System of a Down - Chop Suey!',
+		'file'              => 'Chop Suey!.mp3',
+		'tts'               => 'Chop Suey!.mp3',
+		'lyrics_original'   => 'Wake up (wake up)<br>Grab a brush and put a little make-up<br>Hide the scars to fade away the shake-up (hide the scars to fade away the-)<br>Why\'d you leave the keys upon the table?<br>Here you go create another fable, you wanted to<br>Grab a brush and put a little make-up, you wanted to<br>Hide the scars to fade away the shake-up, you wanted to<br>Why\'d you leave the keys upon the table? You wanted to<br><br>I don\'t think you trust<br>In my self-righteous suicide<br>I cry when angels deserve to die',
+		'lyrics_translated' => 'Herää (herää)<br>Nappaa sivellin ja laita vähän meikkiä<br>Piilota arvet häivytä järkytys (piilota arvet häivytä)<br>Miksi jätit avaimet pöydälle?<br>Tässä menet tekemään uuden tarinan, sinä halusit<br>Nappaa sivellin ja meikkaa vähän, sinä halusit<br>Piilota arvet ja häivytä järkytys, sinä halusit<br>Miksi jätit avaimet pöydälle? Sinä halusit<br><br>En usko, että luotat<br>Itseoikeutettuun itsemurhaani<br>Itken, kun enkelit ansaitsevat kuolla',
+	),
+	array(
+		'name'              => 'Electric Callboy - We Got the Moves',
+		'file'              => 'We Got the Moves.mp3',
+		'tts'               => 'We Got the Moves.mp3',
+		'lyrics_original'   => 'Summer mood, hot sand under my feet<br>Cold beer, cheap wine<br>Yeah, that\'s all that we need<br>We got the moves, we got the moves<br>And everybody\'s like<br>Oh, fuck yeah, let\'s do it again!<br><br>We got the moves, we got the moves<br>And everybody\'s like<br>Yesterday we drank too much, let\'s do it again!<br>We got the groove, we got the groove<br>So everybody, put your hands straight up!<br>Tonight is the night!<br><br>We are young, we are free<br>Sipping on booze on a sunny beach<br>We\'rе alive, we believe<br>That summertime memories<br>Will never fade away',
+		'lyrics_translated' => 'Kesätunnelma, kuuma hiekka jalkojeni alla<br>Kylmää olutta, halpaa viiniä<br>Joo, se on kaikki mitä tarvitsemme<br>Meillä on liikkeet, meillä on liikkeet<br>Ja kaikki ovat että<br>Voi vittu joo, tehdään se uudestaan!<br><br>Meillä on liikkeet, meillä on liikkeet<br>Ja kaikki ovat että<br>Eilen juotiin liikaa, tehdään se uudestaan!<br>Meillä on rytmi, meillä on rytmi<br>Joten kaikki, nostakaa kädet pystyyn!<br>Tänään on se ilta!<br><br>Olemme nuoria, olemme vapaita<br>Juomme viinaa aurinkoisella rannalla.<br>Me elämme, me uskomme<br>Että kesäajan muistot<br>Ei koskaan katoa',
+	),
+	array(
+		'name'              => 'Lord of the Lost - Loreley',
+		'file'              => 'Loreley.mp3',
+		'tts'               => 'Loreley.mp3',
+		'lyrics_original'   => 'She knows her grief is a beast<br>That kills from inside<br>She lifts the souls from their home<br>At aerial high tide<br><br>No moments alone, no sorrow to share<br>In search of a key you know is not there<br>Oh no, Loreley<br>Oh no, Loreley<br><br>With arms to the sky and dreams in the dirt<br>Being stronger than death does not heal the hurt<br>Oh no, Loreley<br>Oh no, Loreley<br><br>Cry, Loreley<br>Cry for Eden, Loreley<br>Cry, Loreley<br>Cry for Eden, Loreley<br>She\'s starved of light, no respite<br>Each breath\'s a burden<br>Time can\'t reverse her grim curse of life eternal',
+		'lyrics_translated' => 'Hän tietää, että hänen surunsa on peto<br>joka tappaa sisältäpäin<br>Hän nostaa sielut kodeistaan<br>Ilmojen nousuveden aikaan<br><br>Ei hetkiä yksin, ei surua jaettavaksi<br>Etsimässä avainta, jota ei ole olemassa.<br>Voi ei, Loreley<br>Voi ei, Loreley<br><br>Kädet taivaaseen ja unelmat multaan päin<br>Kuolemaa vahvempana oleminen ei paranna tuskaa<br>Voi ei, Loreley<br>Voi ei, Loreley<br><br>Itke, Loreley<br>Itke Eedenin puolesta, Loreley<br>Itke, Loreley<br>Itke Edenin puolesta, Loreley<br>Hän on valon nälkäinen, ei ole hengähdystaukoa.<br>Jokainen henkäys on taakka<br>Aika ei voi kääntää hänen ikuisen elämänsä synkkää kirousta',
+	),
+	array(
+		'name'              => 'Green Day - Boulevard of Broken Dreams',
+		'file'              => 'Boulevard of Broken Dreams.mp3',
+		'tts'               => 'Boulevard of Broken Dreams.mp3',
+		'lyrics_original'   => 'I walk a lonely road<br>The only one that I have ever known<br>Don\'t know where it goes<br>But it\'s home to me, and I walk alone<br><br>I walk this empty street<br>On the Boulevard of Broken Dreams<br>Where the city sleeps<br>And I\'m the only one, and I walk alone<br><br>I walk alone, I walk alone<br>I walk alone, I walk<br><br>My shadow\'s the only one that walks beside me<br>My shallow heart\'s the only thing that\'s beating<br>Sometimes, I wish someone out there will find me<br>\'Til then, I walk alone',
+		'lyrics_translated' => 'Kävelen yksinäistä tietä<br>Ainoa jonka olen koskaan tuntenut<br>En tiedä minne se johtaa<br>Mutta se on minulle koti, ja kuljen yksin<br><br>Kävelen tätä tyhjää katua<br>Särkyneiden unelmien bulevardilla<br>Missä kaupunki nukkuu<br>Ja minä olen ainoa, ja kävelen yksin<br><br>Kävelen yksin, kävelen yksin<br>Kävelen yksin, kävelen<br><br>Varjoni on ainoa, joka kävelee vierelläni<br>Vain minun matala sydämeni sykkii<br>Joskus toivon, että joku löytäisi minut<br>Siihen asti kävelen yksin',
+	),
+	array(
+		'name'              => 'Turmion Kätilöt - U.S.C.H!',
+		'file'              => 'U.S.C.H.!.mp3',
+		'tts'               => 'U.S.C.H.!.mp3',
+		'lyrics_original'   => 'Breathe, inhale, savor and swallow<br>Hyperventilate, agony in ecstasy<br>Riot, anarchy, bitches and booze<br>Hail for the new flesh, hanging loose<br><br>Ultimate, synthetic, corrosion, helter-skelter<br>Ultimate, scum, corruption, heaven-sent<br><br>U.S.C.H!<br>U.S.C.H!',
+		'lyrics_translated' => 'Hengitä, hengitä, nauti ja nielaise.<br>Hyperventiloi, tuska ekstaasissa<br>Mellakkaa, anarkiaa, narttuja ja viinaa<br>Eläköön uusi liha, joka roikkuu irrallaan<br><br>Perimmäinen, synteettinen, korroosio, mullin mallin<br>Perimmäinen, saasta, korruptio, taivaan lähettämä<br><br>P.S.K.M.!<br>P.S.K.M.!',
+	),
+	array(
+		'name'              => 'Metallica - Battery',
+		'file'              => 'Battery.mp3',
+		'tts'               => 'Battery.mp3',
+		'lyrics_original'   => 'Lashing out the action, returning the reaction<br>Weak are ripped and torn away<br>Hypnotizing power, crushing all that cower<br>Battery is here to stay<br><br>Smashing through the boundaries<br>Lunacy has found me<br>Cannot stop the battery<br>Pounding out aggression<br>Turns into obsession<br>Cannot kill the battery<br><br>Cannot kill the family<br>Battery is found in me<br><br>Battery<br>Battery',
+		'lyrics_translated' => 'Toiminnan käynnistäminen, reaktion palauttaminen<br>Heikot ovat revenneet ja revitään pois<br>Hypnotisoiva voima, murskaa kaikki nöyrtyvät<br>Paristo on täällä jäädäkseen<br><br>Murskaa rajojen läpi<br>Hulluus on löytänyt minut<br>Ei voi pysäyttää paristoa<br>Aggression purkaminen<br>muuttuu pakkomielteeksi<br>Ei voi tappaa paristoa<br><br>Ei voi tappaa perhettä<br>Paristo löytyy minusta<br><br>Paristo<br>Paristo',
+	),
+	array(
+		'name'              => 'Cascada - Everytime We Touch',
+		'file'              => 'Everytime We Touch.mp3',
+		'tts'               => 'Everytime We Touch.mp3',
+		'lyrics_original'   => 'I still hear your voice when you sleep next to me<br>I still feel your touch in my dreams<br>Forgive me my weakness, but I don\'t know why<br>Without you, it\'s hard to survive<br><br>Cause every time we touch<br>I get this feeling<br>And every time we kiss<br>I swear I could fly<br>Can\'t you feel my heart beat fast<br>I want this to last<br>Need you by my side',
+		'lyrics_translated' => 'Kuulen yhä äänesi, kun nukut vieressäni<br>Tunnen yhä kosketuksesi unissani<br>Anna anteeksi heikkouteni, mutta en tiedä miksi<br>Ilman sinua on vaikea selviytyä<br><br>Koska joka kerta kun kosketamme<br>Saan tämän tunteen<br>Ja joka kerta kun suutelemme<br>Vannon, että voisin lentää<br>Etkö tunne sydämeni lyövän nopeasti<br>Haluan tämän säilyvän<br>Tarvitsen sinut rinnalleni',
+	),
+);
+
+//Counter
+$counter = 1;
+
+//Render array
+foreach ( $translated_songs as $song ) {
+	?>
+	<div class="song">
+		<div class="count">
+			<p><?php echo $counter; ?> / <?php echo count( $translated_songs ); ?></p>
+		</div>
+
+		<div class="lyrics">
+			<div class="translated">
+				<p class="heading">Käännetty</p>
+				<p><?php echo $song['lyrics_translated']; ?></p>
+			</div>
+			<div class="original">
+				<p class="heading">Alkuperäinen</p>
+				<p><?php echo $song['lyrics_original']; ?></p>
+			</div>
+		</div>
+
+		<div class="player">
+			<p>Text to Speech</p>
+			<audio controls>
+				<source src="tts/<?php echo $song['tts']; ?>" type="audio/mpeg">
+			</audio> 
+		</div>
+
+
+		<div class="player">
+			<p>Kappale</p>
+			<audio controls>
+				<source src="music/<?php echo $song['file']; ?>" type="audio/mpeg">
+			</audio> 
+		</div>
+
+		<div class="answer">
+			<a href="#">Näytä vastaus</a>
+			<p><?php echo $song['name']; ?></p>
+		</div>
+	</div>
+	<?php
+	++$counter;
+}
